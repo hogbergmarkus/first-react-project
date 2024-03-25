0eb637e7cd3775b7c14d2bab2c003f0d86debf9c
Markus <144561346+Xeptit@users.noreply.github.com>
2024-03-25 12:36:44 +0100

    Fetching data with GET part 2

--- my-app/src/components/HTTPRequest.js
+++ my-app/src/components/HTTPRequest.js
@@ -1,33 +1,56 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-export class HTTPRequest extends Component {
+export class HTTPRequests extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             posts: [],
+            error: null,
         };
     }
     componentDidMount() {
         axios
-            .get("https://jsonplaceholder.typicode.com/posts")
+            .get("https://jsonplaceholder.typicode.com/posts/")
             .then((response) => {
                 console.log(response);
                 this.setState({
-                    posts: response.data,
+                    posts: Array.isArray(response.data)
+                        ? response.data
+                        : [response.data],
+                });
+            })
+            .catch((error) => {
+                this.setState({
+                    error: error.message,
                 });
             });
     }
-
     render() {
+        const posts = this.state.posts;
         return (
             <div>
-                <h2>Posts: </h2>
-                {JSON.stringify(this.state.posts)}
+                <h2>Posts:</h2>
+                {posts.length ? (
+                    posts.map((post) => (
+                        <div key={post.id}>
+                            <h2>
+                                {post.id}. {post.title}
+                            </h2>
+                            <h4>By User ID {post.userId}</h4>
+                            <p>{post.body}</p>
+                            <hr />
+                        </div>
+                    ))
+                ) : this.state.error ? (
+                    <p>{this.state.error}</p>
+                ) : (
+                    <h4>Loading posts ...</h4>
+                )}
             </div>
         );
     }
 }
 
-export default HTTPRequest;
+export default HTTPRequests;
